49e3018baa92c6b1bf945a5e66ac56498f189045
Hrushikesh Makode <makodehrushikesh@gmail.com>
2021-03-06 18:47:33 +0530

    React-Flask Movie List

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { Container } from "semantic-ui-react";
+import "./App.css";
+import MovieForm from "./components/MovieForm";
+import Movies from "./components/Movies";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [movies, setMovies] = useState([]);
+
+	useEffect(() => {
+		const fetchMovies = () => {
+			fetch("/movies").then((response) =>
+				response.json().then((data) => {
+					console.log(data.movies);
+					setMovies(data.movies);
+				})
+			);
+		};
+		fetchMovies();
+	}, []);
+
+	return (
+		<div>
+			<Container style={{ marginTop: 40 }}>
+				<MovieForm
+					onNewMovie={(movie) =>
+						setMovies((currentMovies) => [movie, ...currentMovies])
+					}
+				/>
+				<Movies movies={movies} />
+			</Container>
+		</div>
+	);
 }
 
 export default App;
